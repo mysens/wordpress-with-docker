b27441d702698b15f12ca4f82def0001bc553bbf
mysens <mysens@126.com>
2022-05-22 13:37:43 +0800

    fix ssl path

--- nginx/init_ssl_cert.sh
+++ nginx/init_ssl_cert.sh
@@ -4,7 +4,7 @@ echo "$0: nginx config ssl closed"
 exit 0
 fi
 cd `dirname $0`
-if [ -d "/cert/.lego" ]; then
+if [ -d "/etc/nginx/cert/.lego" ]; then
 TENCENTCLOUD_SECRET_ID=$LEGO_SECRET_ID \
 TENCENTCLOUD_SECRET_KEY=$LEGO_SECRET_KEY \
 /etc/nginx/cert/lego --email $LEGO_EMAIL --dns tencentcloud --domains="*.$SERVER_NAME" --domains="$SERVER_NAME" \

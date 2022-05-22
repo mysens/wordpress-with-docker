1d453932c7ae1d875c76e53b7190fbe3a1a84e9a
mysens <mysens@126.com>
2022-05-22 13:46:02 +0800

    fix ssl config

--- nginx/init_ssl_cert.sh
+++ nginx/init_ssl_cert.sh
@@ -5,12 +5,8 @@ exit 0
 fi
 cd `dirname $0`
 if [ -d "/etc/nginx/cert/.lego" ]; then
-TENCENTCLOUD_SECRET_ID=$LEGO_SECRET_ID \
-TENCENTCLOUD_SECRET_KEY=$LEGO_SECRET_KEY \
-/etc/nginx/cert/lego --email $LEGO_EMAIL --dns tencentcloud --domains="*.$SERVER_NAME" --domains="$SERVER_NAME" \
+/etc/nginx/cert/lego --email $LEGO_EMAIL --dns $LEGO_DNS --domains="*.$SERVER_NAME" --domains="$SERVER_NAME" \
 renew --days=30 --renew-hook="nginx -s reload" >> autorenew.log
 else
-TENCENTCLOUD_SECRET_ID=$LEGO_SECRET_ID \
-TENCENTCLOUD_SECRET_KEY=$LEGO_SECRET_KEY \
-echo Y | /etc/nginx/cert/lego --email $LEGO_EMAIL --dns tencentcloud --domains="$SERVER_NAME" run
+echo Y | /etc/nginx/cert/lego --email $LEGO_EMAIL --dns $LEGO_DNS --domains="*.$SERVER_NAME" --domains="$SERVER_NAME" run
 fi

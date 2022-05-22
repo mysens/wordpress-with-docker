604b6a835e52bdc54db2808511b1356341948bcd
mysens <mysens@126.com>
2022-05-22 21:33:38 +0800

    fix lego path

--- nginx/init_ssl_cert.sh
+++ nginx/init_ssl_cert.sh
@@ -3,12 +3,12 @@ if [ $USE_SSL -le 0 ];then
 echo "$0: nginx config ssl closed"
 exit 0
 fi
-cd `dirname $0`
-if [ -f "/etc/nginx/cert/.lego/certificates/*.crt" ]; then
+cd `/etc/nginx/cert`
+if [ -f ".lego/certificates/*.crt" ]; then
 echo "$0 crt file exist, try to renew"
-/etc/nginx/cert/lego --email $LEGO_EMAIL --dns $LEGO_DNS --domains="*.$SERVER_NAME" --domains="$SERVER_NAME" \
+./lego --email $LEGO_EMAIL --dns $LEGO_DNS --domains="*.$SERVER_NAME" --domains="$SERVER_NAME" \
 renew --days=30 --renew-hook="nginx -s reload" >> autorenew.log
 else
 echo "$0 crt file not exist, try to create"
-echo Y | /etc/nginx/cert/lego --email $LEGO_EMAIL --dns $LEGO_DNS --domains="*.$SERVER_NAME" --domains="$SERVER_NAME" run
+echo Y | ./lego --email $LEGO_EMAIL --dns $LEGO_DNS --domains="*.$SERVER_NAME" --domains="$SERVER_NAME" run
 fi

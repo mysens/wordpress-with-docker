f6e04f6e634c59b340e6a1ccfda53771c3fa9fea
mysens <mysens@126.com>
2022-05-22 22:31:46 +0800

    change ssl nginx config

--- nginx/init_param.sh
+++ nginx/init_param.sh
@@ -3,9 +3,11 @@ sed -i "s/{SERVER_NAME}/$SERVER_NAME/g" /etc/nginx/conf.d/default.conf;
 if [ -z "${USE_SSL:-}" ];then
 echo "$0: nginx config port 80"
 sed -i "s/{PORT}/80/g" /etc/nginx/conf.d/default.conf;
+sed -i "s/{SSL}//g" /etc/nginx/conf.d/default.conf;
 else
 echo "$0: nginx config port 443"
 sed -i "s/{PORT}/443/g" /etc/nginx/conf.d/default.conf;
+sed -i "s/{SSL}/ ssl/g" /etc/nginx/conf.d/default.conf;
 sed -i "s/# ssl_certificate/ssl_certificate/g" /etc/nginx/conf.d/default.conf; 
 
 echo "# 把所有HTTP请求重定向到HTTPS上
